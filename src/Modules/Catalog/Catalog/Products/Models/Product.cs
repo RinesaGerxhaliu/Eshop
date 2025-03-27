4f27c256c655d696ab4bea9911385993b284a59a
adeabajraktari <ab68295@ubt-uni.net>
2025-03-27 18:07:12 +0100

    Created product domain model and events for handling changes
    Created the product domain model and also create and update methods with events to handle the changes, also added the global using for better modularity.

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,46 @@
+﻿namespace Catalog.Products.Models;
+
+public class Product : Aggregate<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public string Description { get; private set; } = default!;
+
+    //public string ImageFile { get; set; } = default!;
+    public decimal Price { get; private set; } 
+
+    public static Product Create (Guid id, string name, string description, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Description = description,
+            Price = price
+        };
+
+        product.AddDomainEvent(new ProductCreatedEvent(product));
+
+        return product;
+    }
+
+    public void Update (string name, string description, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        Name = name;
+        Description = description;
+        Price = price;
+
+        if (Price != price)
+        {
+            Price = price;
+            AddDomainEvent(new ProductPriceChangedEvent(this));
+        }
+
+    }
+
+}
