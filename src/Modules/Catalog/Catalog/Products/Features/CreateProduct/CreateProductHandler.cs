526c162d02e6b199330788c84f07b8057d54ac75
adeabajraktari <ab68295@ubt-uni.net>
2025-04-08 17:58:18 +0200

    Installed the needed packages to support FluentValidation, created ValidationBehavior class to handle all the needed validations when we add a product
    Implemented this validation behavior in the CreateProductHandler class and also added the needed logging.

--- src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -5,14 +5,30 @@
         : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator() 
+        {
+            RuleFor(x => x.Product.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Product.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
 
-    internal class CreateProductHandler(CatalogDbContext dbContext)
+        }
+    }
+
+    internal class CreateProductHandler
+        (CatalogDbContext dbContext, 
+        ILogger<CreateProductHandler> logger)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
 
         public async Task<CreateProductResult> Handle(CreateProductCommand command,
         CancellationToken cancellationToken)
         {
+
+            //logging part
+            logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+
+            //actual business logic part
             var product = CreateNewProduct(command.Product);
 
             dbContext.Products.Add(product);
