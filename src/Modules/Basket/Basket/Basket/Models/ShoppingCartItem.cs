069474a0559857810c627204e30ad4d61f9e1387
adeabajraktari <ab68295@ubt-uni.net>
2025-04-11 01:05:20 +0200

    Created JsonConverter classes for Shopping cart items and shopping cart to use in cached basket repo

--- src/Modules/Basket/Basket/Basket/Models/ShoppingCartItem.cs
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCartItem.cs
@@ -21,4 +21,16 @@ public class ShoppingCartItem : Entity<Guid>
         Price = price;
         ProductName = productName;
     }
+
+    [JsonConstructor]
+    public ShoppingCartItem(Guid id, Guid shoppingCartId, Guid productId, int quantity, string color, decimal price, string productName)
+    {
+        Id = id;
+        ShoppingCartId = shoppingCartId;
+        ProductId = productId;
+        Quantity = quantity;
+        Color = color;
+        Price = price;
+        ProductName = productName;
+    }
 }
