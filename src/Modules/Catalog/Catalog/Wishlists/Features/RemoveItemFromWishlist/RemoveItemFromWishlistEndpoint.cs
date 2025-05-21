3cef57f2fcd5a69afad79454733ab6240d7a2475
adeabajraktari <149432035+adeabajraktari@users.noreply.github.com>
2025-05-21 02:33:56 +0200

    Created Handler & Enpoint for AddItemIntoWishlist Feature

---
+++ src/Modules/Catalog/Catalog/Wishlists/Features/RemoveItemFromWishlist/RemoveItemFromWishlistEndpoint.cs
@@ -0,0 +1,30 @@
+﻿namespace Catalog.Wishlists.Features.RemoveItemFromWishlist;
+
+public record RemoveItemFromWishlistResponse(Guid Id);
+
+public class RemoveItemFromWishlistEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/wishlist/{customerId}/items/{productId}",
+            async ([FromRoute] string customerId,
+                   [FromRoute] Guid productId,
+                   ISender sender) =>
+            {
+                var command = new RemoveItemFromWishlistCommand(customerId, productId);
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<RemoveItemFromWishlistResponse>();
+
+                return Results.Ok(response);
+            })
+        .Produces<RemoveItemFromWishlistResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Remove Item From Wishlist")
+        .WithDescription("Remove Item From Wishlist")
+        .RequireAuthorization();
+    }
+}
+
+
---
+++ src/Modules/Catalog/Catalog/Wishlists/Features/RemoveItemFromWishlist/RemoveItemFromWishlistHandler.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Wishlists.Repositories;
+
+namespace Catalog.Wishlists.Features.RemoveItemFromWishlist;
+
+public record RemoveItemFromWishlistCommand(string CustomerId, Guid ProductId)
+    : ICommand<RemoveItemFromWishlistResult>;
+
+public record RemoveItemFromWishlistResult(Guid Id);
+
+public class RemoveItemFromWishlistCommandValidator : AbstractValidator<RemoveItemFromWishlistCommand>
+{
+    public RemoveItemFromWishlistCommandValidator()
+    {
+        RuleFor(x => x.CustomerId).NotEmpty().WithMessage("CustomerId is required");
+        RuleFor(x => x.ProductId).NotEmpty().WithMessage("ProductId is required");
+    }
+}
+
+internal class RemoveItemFromWishlistHandler(IWishlistRepository repository)
+    : ICommandHandler<RemoveItemFromWishlistCommand, RemoveItemFromWishlistResult>
+{
+    public async Task<RemoveItemFromWishlistResult> Handle(RemoveItemFromWishlistCommand command, CancellationToken cancellationToken)
+    {
+        var wishlist = await repository.GetWishlist(command.CustomerId, false, cancellationToken);
+
+        wishlist.RemoveItem(command.ProductId);
+
+        await repository.SaveChangesAsync(command.CustomerId, cancellationToken);
+
+        return new RemoveItemFromWishlistResult(wishlist.Id);
+    }
+}
+
