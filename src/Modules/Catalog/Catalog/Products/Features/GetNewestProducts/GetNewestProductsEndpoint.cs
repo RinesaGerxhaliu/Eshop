b87d98c1c7f45c6698c91f7afdbdb8558650cc27
Altina Braha <149594836+ab66201@users.noreply.github.com>
2025-05-25 01:40:19 +0200

    Created Handler and endpoint to get 4 newest products

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetNewestProducts/GetNewestProductsEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Catalog.Products.Features.GetProducts;
+
+public class GetNewestProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/newest", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetNewestProductsQuery());
+            return Results.Ok(result);
+        })
+        .WithName("GetNewestProducts")
+        .Produces<List<ProductDTO>>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get 4 newest products")
+        .WithDescription("Returns the 4 most recently added products based on ID.");
+    }
+}
---
+++ src/Modules/Catalog/Catalog/Products/Features/GetNewestProducts/GetNewestProductsHandler.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Products.Models;
+using Mapster;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Products.Features.GetProducts;
+
+public record GetNewestProductsQuery : IQuery<List<ProductDTO>>;
+
+internal class GetNewestProductsHandler : IQueryHandler<GetNewestProductsQuery, List<ProductDTO>>
+{
+    private readonly CatalogDbContext _db;
+
+    public GetNewestProductsHandler(CatalogDbContext db) => _db = db;
+
+    public async Task<List<ProductDTO>> Handle(GetNewestProductsQuery request, CancellationToken ct)
+    {
+        var products = await _db.Products
+            .AsNoTracking()
+            .Include(p => p.Image)
+            .OrderByDescending(p => p.CreatedAt)
+            .Take(4)
+            .ToListAsync();
+
+        return products.Adapt<List<ProductDTO>>();
+    }
+}
